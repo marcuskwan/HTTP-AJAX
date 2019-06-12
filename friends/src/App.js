c29d87621505d0038b3307b27ca878b7add47360
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-13 06:03:18 +1000

    changed app into class, created friendlist and friend class components

---
+++ friends/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+import FriendList from "./components/FriendList";
+import axios from "axios";
+
+export default class App extends Component {
+  state = {
+    friends: []
+  };
+  componentDidMount() {
+    // gets data
+    axios
+      .get("http://localhost:5000/friends")
+      .then(result => {
+        console.log(result);
+        this.setState({ friends: result.data });
+      })
+      .catch(err => {
+        console.log(err);
+        // this.setState({
+        //   error: err.response.message
+        // });
+      });
+
+    //     this.setState({ items: data });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        hi
+        <FriendList />
+      </div>
+    );
+  }
+}
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FriendList extends Component {
+    render() {
+        return (
+            <div>
+                hi
+            </div>
+        )
+    }
+}
+
+export default FriendList
